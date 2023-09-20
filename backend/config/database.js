57ff9117d42f4829efba01b3b2c6657953e7a4c7
Rapheal Ajiboye <raphoooo95@gmail.com>
2023-09-20 08:56:28 +0100

    fixing db url

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,13 +2,22 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
 
-const connectDb = () =>{
-    mongoose.connect(process.env.DB_URI, {
+const connectDb = () => {
+  mongoose
+    .connect(
+      process.env.NODE_ENV == 'DEVELOPMENT'
+        ? process.env.DB_LOCAL_URI
+        : process.env.DB_URI,
+      {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    }).then(con =>{
-        console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
-    })
-}
+      }
+    )
+    .then((con) => {
+      console.log(
+        `MongoDB Database connected with HOST: ${con.connection.host}`
+      );
+    });
+};
 
 module.exports = connectDb;
